df18f05bca259689f401c1a15c425a757d06c9e1
kellyspiano <kellyspianoau@gmail.com>
2011-12-30 20:28:41 +1000

    First import.

---
+++ ex3.py
@@ -0,0 +1,29 @@
+# Tells that I am about to count my chickens
+print "I will now count my chickens:"
+
+# Counts the hens by performing simple mathematics
+print "Hens", 25.0 + 30.0 / 6.0
+# Counts the Roosters by performing simple mathematics
+print "Roosters", 100. - 25. * 3. % 4.
+
+# Counts the eggs by performing simple mathematics
+print "Now I will count the eggs:"
+
+# Performs the calculations to count the eggs
+print 3. + 2. + 1. - 5. + 4. % 2. - 1. / 4. + 6.
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3. + 2. < 5. - 7.
+
+print "What is 3 + 2?", 3. + 2.
+print "What is 5 - 7?", 5. - 7.
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <+ -2
+
